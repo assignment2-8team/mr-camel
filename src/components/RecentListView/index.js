0c472119b7a357733a2f5ab7aa215acef889f570
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-08-01 00:19:30 +0900

    chore: update remote main

---
+++ src/components/RecentListView/index.js
@@ -0,0 +1,39 @@
+import { Component } from "react";
+import { withRouter } from "react-router";
+import Card from "components/Card";
+import "./style.css";
+import handleOnClickProduct from "utils/handleOnClick/handleOnClickProduct";
+
+class RecentListView extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const { sortMethod, brandList } = this.props;
+
+    if (!brandList) return null;
+
+    if (sortMethod === "price") {
+        let sortedBrandList = [...brandList];
+        sortedBrandList.sort((x, y) => {return x.price - y.price});
+        return (
+            <div className="recentList-view">
+              {sortedBrandList.map(item => (
+                <Card key={item.id} productInfo={item} handleOnClick={(product)=>handleOnClickProduct(product, this.props.history)} />
+              ))}
+            </div>
+        );
+    }
+
+    return (
+      <div className="recentList-view">
+        {brandList.map(item => (
+          <Card key={item.id} productInfo={item} handleOnClick={(product)=>handleOnClickProduct(product, this.props.history)} />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default withRouter(RecentListView);
---
+++ src/pages/Main/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import "./style.css";
+import Header from "components/Header";
+import Card from "components/Card";
+import PRODUCT_LIST from "utils/constants/PRODUCT_LIST";
+import handleOnClickProduct from "utils/handleOnClick/handleOnClickProduct";
+
+class Main extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const history = window.localStorage.getItem("inquiryHistory");
+
+    return (
+      <div className="main-page">
+        <Header />
+        <div className="main-page-content">
+          <div className="fullList-view">
+            <div className="all-product-render">
+              {PRODUCT_LIST.map(e => (
+                <Card productInfo={e} handleOnClick={(product)=>handleOnClickProduct(product, this.props.history)} />
+              ))}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Main;
