6944ce774a5077835193e637628def22d7f06e72
yeoncho <yeoncho@student.42seoul.kr>
2021-07-31 12:39:24 +0900

    ✨feat: App에 localStorage 초기 세팅 추가

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,6 +4,11 @@ import UserRouter from "../../router";
 
 
 class App extends Component {
+    constructor(){
+        super()
+        window.localStorage.setItem("inquiryHistory", JSON.stringify({"recent": []}));
+        window.localStorage.setItem("apathy",  JSON.stringify({}));
+    }
     render() {
         return (
           <>
