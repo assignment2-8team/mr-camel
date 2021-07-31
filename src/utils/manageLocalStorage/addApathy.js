4ce12fabf30d10e3ff2ae271c4b2a3dac0277dd0
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-07-31 22:30:16 +0900

    Updates

--- src/utils/manageLocalStorage/addApathy.js
+++
@@ -1,6 +0,0 @@
-const addApathy = (productId) => {
-    const apathy = JSON.parse(window.localStorage.getItem("apathy"));
-    window.localStorage.setItem("apathy", JSON.stringify([ ...apathy,  productId]));
-}
-
-export default addApathy;
