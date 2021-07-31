bfaa2134fbc95dc48c44493ed073895109ba6138
HEEYOUNG SEO <hseo@HEEYOUNGs-iMac.local>
2021-07-31 23:56:29 +0900

    Updates

--- src/utils/manageHistory/manageHistoryPush.js
+++
@@ -1,8 +0,0 @@
-const manageHistoryPush = (productInfo, history) => {
-    window.sessionStorage.setItem("currentProductInfo", JSON.stringify(productInfo));
-    history.push({
-        pathname: "/product",
-    });
-};
-
-export default manageHistoryPush;
